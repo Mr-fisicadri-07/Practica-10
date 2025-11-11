1668337f5ce73c55d28b56e919b2b55d60fce37c
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-11-11 11:41:12 +0100

    Corregido

--- animale_marco.py
+++ animale_marco.py
@@ -1,79 +1,4 @@
-class Animal:
-    """Clase base que almacena el nombre y el sonido."""
-    def __init__(self, nombre, sonido):
-        self.nombre = nombre
-        self.sonido = sonido
-
-class Animales_Domesticos(Animal): # (Nota: Quité la tilde, es mejor evitar acentos en nombres de clases)
-    """
-    Clase intermedia. DEBE aceptar los argumentos de sus hijos
-    y pasarlos a su padre (Animal).
-    """
-    def __init__(self, nombre, sonido):
-        # 1. Recibe 'nombre' y 'sonido' (de Perro)
-        # 2. Los pasa hacia "arriba" (a Animal)
-        super().__init__(nombre, sonido)
-        
-        # Opcional: Aquí puedes añadir lógica solo para domésticos
-        # self.es_domestico = True 
-
-class Perro(Animales_Domesticos):
-    
-    def __init__(self):
-        # Pasa los valores "Perro" y "guau guau"
-        # a su clase padre (Animales_Domesticos)
-        super().__init__("Soy un perro", "Quería decir, guau guau")
-
-class Gato(Animales_Domesticos):
-    
-    def __init__(self):
-        # Pasa los valores "Perro" y "guau guau"
-        # a su clase padre (Animales_Domesticos)
-        super().__init__("Soy un gato", "Perdón, miau miau")
-
-
-class Hamster(Animales_Domesticos):
-    
-    def __init__(self):
-        # Pasa los valores "Perro" y "guau guau"
-        # a su clase padre (Animales_Domesticos)
-        super().__init__("Soy un hamster", "quería decir *roe algo*")
-
-class Periquito(Animales_Domesticos):
-    
-    def __init__(self):
-        # Pasa los valores "Perro" y "guau guau"
-        # a su clase padre (Animales_Domesticos)
-        super().__init__("Soy un periquito", "perdón, pío pío")
-
-class Perro(Animales_Domesticos):
-    
-    def __init__(self):
-        # Pasa los valores "Perro" y "guau guau"
-        # a su clase padre (Animales_Domesticos)
-        super().__init__("Perro"    , "guau guau")
-class Animal:
-    def __init__(self, nombre, sonido):
-        self.nombre = nombre
-        self.sonido = sonido
-class animales_granja(Animal):
-    def __init__(self, nombre, sonido):
-        super().__init__(nombre, sonido)
-class Vaca(animales_granja):
-    def __init__(self, nombre):
-        super().__init__(nombre, "Muu")
-class Cerdo(animales_granja):
-    def __init__(self, nombre):
-        super().__init__(nombre, "Oink")
-class Oveja(animales_granja):
-    def __init__(self, nombre):
-        super().__init__(nombre, "Bee")
-class Gallo(animales_granja):
-    def __init__(self, nombre):
-        super().__init__(nombre, "kikiriki")
-class  cabra(animales_granja):
-    def __init__(self, nombre):
-        super().__init__(nombre, "Mee")
+from animales import Animal
 
 class AnimalSalvaje(Animal):
     def __init__(self, nombre, sonido):
@@ -82,25 +7,31 @@ class AnimalSalvaje(Animal):
         self.sonido = sonido    
 
 class Leon (AnimalSalvaje):
-    def __init__(self, nombre, sonido, caracteristica="Rey de la selva"):
-        super().__init__(nombre, sonido)
+    def __init__(self, nombre, sonido, caracteristica="Rey de la selva", ubicacion="selva"):
+        super().__init__(nombre, "roar")
         self.caracteristica = caracteristica
-        sonido = "r"
+        self.ubicacion = ubicacion
 
 class mono (AnimalSalvaje):
-    def __init__(self, nombre, sonido , caracteristica="Muy jugueton"):
-        super().__init__(nombre, sonido)
-    
+    def __init__(self, nombre, sonido , caracteristica="Muy jugueton", ubicacion="selva"):
+        super().__init__(nombre, "U u a a")
+        self.caracteristica = caracteristica
+        self.ubicacion = ubicacion
 
 class lobo (AnimalSalvaje):
-    def __init__(self, nombre, sonido , caracteristica="Cazador en manada"):
-        super().__init__(nombre, sonido)    
+    def __init__(self, nombre, sonido , caracteristica="Cazador en manada", ubicacion="montaña"):
+        super().__init__(nombre, "Auuuuuuuuuuuuuuu")    
+        self.caracteristica = caracteristica
+        self.ubicacion = ubicacion
 
 class elefante (AnimalSalvaje):
-    def __init__(self, nombre, sonido , caracteristica="El animal terrestre mas grande"):
-        super().__init__(nombre, sonido)
+    def __init__(self, nombre, sonido , caracteristica="El animal terrestre mas grande", ubicacion="sabana"):
+        super().__init__(nombre, "Patum patum patum")
+        self.caracteristica = caracteristica
+        self.ubicacion = ubicacion
 
 class hiena (AnimalSalvaje):
-    def __init__(self, nombre, sonido , caracteristica="Risa siniestra"):
-        super().__init__(nombre, sonido)
-        super().__init__(nombre, "Baaaaaaa")
+    def __init__(self, nombre, caracteristica="Risa siniestra", ubicacion="bosque"):
+        super().__init__(nombre, "Auuuuuuuuuuuuuuuuuu")
+        self.caracteristica = caracteristica
+        self.ubicacion = ubicacion
