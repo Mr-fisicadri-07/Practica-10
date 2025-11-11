85c4eaf59e0a8380a759bb311f1b88d013699f15
Jorge <jorgefdo.martinezfdez@gmail.com>
2025-11-11 10:47:17 +0100

    Refactor animal classes for better structure

--- animales.py
+++ animales.py
@@ -52,3 +52,25 @@ class Perro(Animales_Domesticos):
         # Pasa los valores "Perro" y "guau guau"
         # a su clase padre (Animales_Domesticos)
         super().__init__("Perro"    , "guau guau")
+class Animal:
+    def __init__(self, nombre, sonido):
+        self.nombre = nombre
+        self.sonido = sonido
+class animales_granja(Animal):
+    def __init__(self, nombre, sonido):
+        super().__init__(nombre, sonido)
+class Vaca(animales_granja):
+    def __init__(self, nombre):
+        super().__init__(nombre, "Muu")
+class Cerdo(animales_granja):
+    def __init__(self, nombre):
+        super().__init__(nombre, "Oink")
+class Oveja(animales_granja):
+    def __init__(self, nombre):
+        super().__init__(nombre, "Bee")
+class Gallo(animales_granja):
+    def __init__(self, nombre):
+        super().__init__(nombre, "kikiriki")
+class  cabra(animales_granja):
+    def __init__(self, nombre):
+        super().__init__(nombre, "Baaaaaaa")
